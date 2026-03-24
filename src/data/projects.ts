e77b6b1b6c2720f049944ae2233315ec0e6b9e8b
Milan Bairagi <nmilan626@gmail.com>
2026-03-24 21:18:27 +0545

    feat: add project data module with details for WaveLine, LeafLock, EstateEase, and BidThrill

---
+++ src/data/projects.ts
@@ -0,0 +1,34 @@
+export const projectData = [
+  {
+    title: "WaveLine",
+    description: "A modern, real-time chat application built with Django (Backend) and React (Frontend) that enables seamless communication through WebSocket connections.",
+    image: "project-images/WaveLine.png",
+    technologies: ["Django", "React", "PostgreSQL", "DRF", "Tailwind CSS", "JWT", "WebSocket"],
+    liveUrl: "https://wave-line.vercel.app",
+    githubUrl: "https://github.com/milanbairagi/WaveLine"
+  },
+  {
+    title: "LeafLock",
+    description: "A secure password manager built with Django REST API and React. It allows users to store, manage, and generate strong passwords with features like encryption and user authentication.",
+    image: "project-images/LeafLock.png",
+    technologies: ["React", "Django", "PostgreSQL", "DRF", "Tailwind CSS", "JWT"],
+    liveUrl: "https://leaf-lock.vercel.app",
+    githubUrl: "https://github.com/milanbairagi/LockLeaf"
+  },
+  {
+    title: "EstateEase",
+    description: "A full-stack e-commerce solution built with Django REST API and React. Features include user authentication, payment integration, order management, and admin dashboard.",
+    image: "project-images/EstateEase.png",
+    technologies: ["React", "Django", "PostgreSQL", "DRF", "Tailwind CSS", "Cloudinary", "JWT"],
+    liveUrl: "https://estate-ease-rho.vercel.app",
+    githubUrl: "https://github.com/milanbairagi/EstateEase"
+  },
+  {
+    title: "BidThrill",
+    description: "A e-commerce bidding site where user can upload or bid on the products. Features include user authentication, watchlist, comments and bidding system.",
+    image: "project-images/BidThrill.png",
+    technologies: ["Django", "Bootstrap", "SQLite"],
+    liveUrl: "",
+    githubUrl: "https://github.com/milanbairagi/bidthrill"
+  },
+];
