2ed7a8d065328eaa234e8a7e44f15afea6d1523c
Milan Bairagi <nmilan626@gmail.com>
2026-03-24 21:00:09 +0545

    feat: add skills data module with frontend, backend, and other skills

---
+++ src/data/skills.ts
@@ -0,0 +1,68 @@
+export const frontendSkills = [
+  {
+    "name": "React",
+    "icon": null
+  },
+  {
+    "name": "JavaScript",
+    "icon": null
+  },
+  {
+    "name": "HTML",
+    "icon": null
+  },
+  {
+    "name": "CSS",
+    "icon": null
+  },
+  {
+    "name": "Tailwind CSS",
+    "icon": null
+  },
+  {
+    "name": "Figma",
+    "icon": null
+  }
+];
+
+export const backendSkills = [
+  {
+    "name": "Django",
+    "icon": null
+  },
+  {
+    "name": "Django Rest Framework",
+    "icon": null
+  },
+  {
+    "name": "MySQL",
+    "icon": null
+  },
+  {
+    "name": "PostgreSQL",
+    "icon": null
+  },
+  {
+    "name": "SQLite",
+    "icon": null
+  }
+];
+
+export const otherSkills = [
+  {
+    "name": "Git",
+    "icon": null
+  },
+  {
+    "name": "GitHub",
+    "icon": null
+  },
+  {
+    "name": "Docker",
+    "icon": null
+  },
+  {
+    "name": "Linux",
+    "icon": null
+  }
+]
