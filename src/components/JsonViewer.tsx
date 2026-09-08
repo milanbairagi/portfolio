9f3cbab782b37291af33bb508a02c6e3d53a25da
Milan Bairagi <nmilan626@gmail.com>
2026-09-08 16:19:58 +0545

    feat: add `JsonViewer` component

---
+++ src/components/JsonViewer.tsx
@@ -0,0 +1,23 @@
+import { Highlight, themes } from "prism-react-renderer"
+
+const JsonViewer = ({ jsonString }: { jsonString: string }) => {
+
+  return (
+    <Highlight theme={themes.gruvboxMaterialDark} code={jsonString} language="json">
+      {({ style, tokens, getLineProps, getTokenProps }) => (
+        <pre style={{...style, backgroundColor: "transparent"}}>
+          {tokens.map((line, i) => (
+            <div key={i} {...getLineProps({ line })}>
+              {/* <span>{i + 1}</span> */}
+              {line.map((token, key) => (
+                <span key={key} {...getTokenProps({ token })} />
+              ))}
+            </div>
+          ))}
+        </pre>
+      )}
+    </Highlight>
+  );
+};
+
+export default JsonViewer;
