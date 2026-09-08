582163da8b1f3b1d5e6d22b2ee5fd3f8d87530df
Milan Bairagi <nmilan626@gmail.com>
2026-09-08 19:00:16 +0545

    style: change text to small

--- src/components/JsonViewer.tsx
+++ src/components/JsonViewer.tsx
@@ -5,7 +5,7 @@ const JsonViewer = ({ jsonString }: { jsonString: string }) => {
   return (
     <Highlight theme={themes.gruvboxMaterialDark} code={jsonString} language="json">
       {({ style, tokens, getLineProps, getTokenProps }) => (
-        <pre style={{...style, backgroundColor: "transparent"}}>
+        <pre style={{...style, backgroundColor: "transparent"}} className="text-sm">
           {tokens.map((line, i) => (
             <div key={i} {...getLineProps({ line })}>
               {/* <span>{i + 1}</span> */}
